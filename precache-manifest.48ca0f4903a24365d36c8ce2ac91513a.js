91f31777c8f258e15bae8a9cb8d337ff41df5a4c
Sebastian Torres Retana <storres2@uw.edu>
2019-02-21 01:25:07 -0800

    Updates

---
+++ precache-manifest.48ca0f4903a24365d36c8ce2ac91513a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hcde498-test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fcf35d2e6e4308c59a02",
+    "url": "/hcde498-test/static/js/runtime~main.fcf35d2e.js"
+  },
+  {
+    "revision": "e271653b24afa2fbf916",
+    "url": "/hcde498-test/static/js/main.e271653b.chunk.js"
+  },
+  {
+    "revision": "25e415d7515b32dc48be",
+    "url": "/hcde498-test/static/js/2.25e415d7.chunk.js"
+  },
+  {
+    "revision": "e271653b24afa2fbf916",
+    "url": "/hcde498-test/static/css/main.2c9723c1.chunk.css"
+  },
+  {
+    "revision": "e3fdb9542c622c1e1cef4e866fafe68f",
+    "url": "/hcde498-test/index.html"
+  }
+];
